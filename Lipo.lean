2cdc658e3fdbfc31f05ec5870f43c12115bed558
Ryheff24 <40723346+Ryheff24@users.noreply.github.com>
2026-02-07 21:59:55 -0500

    init

---
+++ Lipo.lean
@@ -0,0 +1,10 @@
+-- This module serves as the root of the `Lipo` library.
+-- Import modules here that should be built as part of the library.
+
+import Lipo.approximationgreed
+import Lipo.argmax
+import Lipo.coverage
+import Lipo.gini
+import Lipo.maximized
+import Lipo.submodular
+
